85a6a2246d3c5f37eb79453e8a79f98eb94bff43
Arthur Bulgakov <bulgakov.arthur@yandex.ru>
2021-05-20 14:36:58 +0300

    Добавлен функционал JS + мелкие правки по проекту

--- scripts/script.js
+++ scripts/script.js
@@ -6,20 +6,20 @@ let close_button = document.querySelector('.popup__close-button');
 let save_button = document.querySelector('.popup__save-button');
 let popupName = document.querySelector('.popup__name')
 let popupDescription = document.querySelector('.popup__description')
+let elements = document.querySelectorAll('.element')
 
 function openPopup () {
   popup.classList.add('popup_opened');
   popupName.value = adventerName.textContent;
   popupDescription.value = adventerDescription.textContent;
-}
+};
 
 function closePopup () {
   popup.classList.remove('popup_opened');
-}
+};
 
 function saveParams (evt) {
   evt.preventDefault();
-  console.log('кнопка нажалась')
   if (popupName.value !== '' && popupDescription.value !== '') {
     closePopup();
     adventerName.textContent = popupName.value;
@@ -35,6 +35,16 @@ function saveParams (evt) {
   };
 }
 
+let makeLike = (event) => {
+  let el = event.currentTarget
+  el.classList.toggle('element_liked')
+}
+
+elements.forEach(element => {
+  let like = element.querySelector('.element__like')
+  like.addEventListener('click', (event) => makeLike(event))
+})
+
 edit_button.addEventListener('click', openPopup);
 close_button.addEventListener('click', closePopup);
-save_button.addEventListener('click', saveParams);
+save_button.addEventListener('submit', saveParams);
