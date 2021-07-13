b7b53e66e26d946eba2be0802a86ad4abf3b095d
Arthur Bulgakov <bulgakov.arthur@yandex.ru>
2021-07-13 13:32:39 +0300

    Добавлены валидации

---
+++ scripts/validate.js
@@ -0,0 +1,73 @@
+const showInputError = (formElement, inputElement, errorMessage, config) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.add(config.errorClass);
+  errorElement.textContent = errorMessage;
+}
+
+const hideInputError = (formElement, inputElement, config) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.remove(config.errorClass);
+  errorElement.textContent = '';
+}
+
+const checkValidity = (formElement, inputElement, config) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, config);
+  } else {
+    hideInputError(formElement, inputElement, config);
+  }
+}
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    console.log(inputElement.validity);
+    return !inputElement.validity.valid;
+  })
+}
+
+const toggleButtonState = (inputList, buttonElement, config) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(config.inactiveButtonClass);
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.classList.remove(config.inactiveButtonClass);
+    buttonElement.removeAttribute('disabled');
+  }
+}
+
+const setEventListeners = (formElement, config) => {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, config);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkValidity(formElement, inputElement, config);
+      toggleButtonState(inputList, buttonElement, config);
+    })
+  })
+}
+
+
+const handleFormInput = (config, form) => {
+    form.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    })
+    setEventListeners(form, config);
+}
+
+const errorCleaner = (config) => {
+  const errorFields = Array.from(document.querySelectorAll(`.${config.inputErrorClass}`));
+  errorFields.forEach((errorField) => {
+    errorField.classList.remove(`.${config.errorClass}`);
+    errorField.textContent = '';
+  })
+}
+
+const enableValidation = (config) => {
+  const forms = Array.from(document.querySelectorAll(config.formSelector));
+  forms.forEach((form) => {
+    handleFormInput(config, form);
+  })
+}
+
+enableValidation(config);
